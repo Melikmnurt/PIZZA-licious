90372e8b1e66d4cfcae6d6c44727c67892cf2633
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-28 14:27:06 -0500

    Inital Commit... Array Lists Added

--- src/main/java/com/pizzalicious/models/Pizza.java
+++ src/main/java/com/pizzalicious/models/Pizza.java
@@ -1,4 +1,28 @@
 package com.pizzalicious.models;
 
+import java.util.ArrayList;
+
 public class Pizza {
+
+    // pizza size: 8", 12", 16"
+    private String size;
+
+    // thin, regular, thick, cauliflower
+    private String crustType;
+
+    // stuffed crust yes/no
+    private boolean stuffedCrust;
+
+    // premium meat toppings
+    private ArrayList<String> meats;
+
+    // premium cheese toppings
+    private ArrayList<String> cheeses;
+
+    // regular toppings
+    private ArrayList<String> regularToppings;
+
+    // sauces
+    private ArrayList<String> sauces;
+
 }

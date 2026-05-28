4c712a5f28b6c4c57c5bcff22d9d2b07a0bcb999
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-28 15:07:50 -0500

    Pizza Done

--- src/main/java/com/pizzalicious/models/Pizza.java
+++ src/main/java/com/pizzalicious/models/Pizza.java
@@ -2,27 +2,78 @@ package com.pizzalicious.models;
 
 import java.util.ArrayList;
 
+// Represents a pizza ordered by a customer
 public class Pizza {
 
-    // pizza size: 8", 12", 16"
-    private String size;
-
-    // thin, regular, thick, cauliflower
-    private String crustType;
-
-    // stuffed crust yes/no
+    // Pizza information
+    private PizzaSize size;
+    private CrustType crustType;
     private boolean stuffedCrust;
 
-    // premium meat toppings
-    private ArrayList<String> meats;
+    // Pizza toppings
+    private ArrayList<Topping> meats;
+    private ArrayList<Topping> cheeses;
+    private ArrayList<Topping> regularToppings;
+    private ArrayList<Topping> sauces;
 
-    // premium cheese toppings
-    private ArrayList<String> cheeses;
+    // Constructor
+    public Pizza(PizzaSize size, CrustType crustType, boolean stuffedCrust) {
 
-    // regular toppings
-    private ArrayList<String> regularToppings;
+        this.size = size;
+        this.crustType = crustType;
+        this.stuffedCrust = stuffedCrust;
 
-    // sauces
-    private ArrayList<String> sauces;
+        meats = new ArrayList<>();
+        cheeses = new ArrayList<>();
+        regularToppings = new ArrayList<>();
+        sauces = new ArrayList<>();
+    }
 
+    // Getters
+
+    public PizzaSize getSize() {
+        return size;
+    }
+
+    public CrustType getCrustType() {
+        return crustType;
+    }
+
+    public boolean isStuffedCrust() {
+        return stuffedCrust;
+    }
+
+    public ArrayList<Topping> getMeats() {
+        return meats;
+    }
+
+    public ArrayList<Topping> getCheeses() {
+        return cheeses;
+    }
+
+    public ArrayList<Topping> getRegularToppings() {
+        return regularToppings;
+    }
+
+    public ArrayList<Topping> getSauces() {
+        return sauces;
+    }
+
+    // Methods for adding toppings
+
+    public void addMeat(Topping topping) {
+        meats.add(topping);
+    }
+
+    public void addCheese(Topping topping) {
+        cheeses.add(topping);
+    }
+
+    public void addRegularTopping(Topping topping) {
+        regularToppings.add(topping);
+    }
+
+    public void addSauce(Topping topping) {
+        sauces.add(topping);
+    }
 }

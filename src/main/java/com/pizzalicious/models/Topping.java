88db52c205c9dad5cfba7445365a6e7a58fc5f12
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-28 15:00:05 -0500

    Topping Done

--- src/main/java/com/pizzalicious/models/Topping.java
+++ src/main/java/com/pizzalicious/models/Topping.java
@@ -1,4 +1,32 @@
 package com.pizzalicious.models;
 
+// Represents a topping and how many times it was added
 public class Topping {
+
+    // Name of the topping
+    private String name;
+
+    // Quantity selected by the customer
+    private int quantity;
+
+    // Constructor
+    public Topping(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    // Returns the topping name
+    public String getName() {
+        return name;
+    }
+
+    // Returns the quantity
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Updates the quantity
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
